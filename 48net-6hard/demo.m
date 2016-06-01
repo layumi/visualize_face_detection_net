42d9ef8ac21d4449fdc05e9e4a178e7a82c1a339
layumi <12307130083@fudan.edu.cn>
2016-06-01 21:18:59 +0800

    update pic

--- 48net-6hard/demo.m
+++ 48net-6hard/demo.m
@@ -7,5 +7,5 @@ net = vl_simplenn_tidy(net) ;
 net = dagnn.DagNN.fromSimpleNN(net, 'canonicalNames', true) ;
 net.addLayer('error', dagnn.Loss('loss', 'classerror'), ...
     {'x16','label'}, 'error') ;
-%draw_full_net(net,'48net');
-draw_simple_net(net,'48net');
+draw_full_net(net,'48net');
+%draw_simple_net(net,'48net');
